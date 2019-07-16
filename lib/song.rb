00df5e3e90aca52f11e954df03e56453bfedd1bf
Amy Fogelman <abfnj1@gmail.com>
2019-07-16 20:59:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,21 +40,24 @@ def self.alphabetical
  @@all.sort_by{|song| song.name}
 end
 
-def self.new_from_filename(filename)
-  row = filename
-  data = row.split(" - ")
-  artist_name = data[0]
-  song_name = data[1].gsub(".mp3", "")
-  song = self.new
-  song.name = song_name
-  song.artist_name = artist_name
-  song
-end
-
-def self.create_from_filename(filename)
-  song = self.new_from_filename(filename)
-  song.save
-  song
+  def self.new_from_filename(filename)
+    song_array = filename.split(" - ")
+    song_array[1] = song_array[1].chomp(".mp3")
+    song = self.new
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
+    song
   end
- end
+
+  def self.create_from_filename(filename)
+    result = self.new_from_filename(filename)
+    song = self.create
+    song.name = result.name
+    song.artist_name = result.artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
